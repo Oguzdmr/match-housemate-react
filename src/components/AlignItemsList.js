69bb1dead63db117f1d5638cfedca28bed575742
Oguzdmr <doguzhan476@gmail.com>
2023-10-03 21:54:15 +0300

    create match service and update AlignItemsList

--- src/components/AlignItemsList.js
+++ src/components/AlignItemsList.js
@@ -6,8 +6,22 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import Match from "../services/MatchService";
+
 
 export default function AlignItemsList({ title }) {
+  const [items, setItems] = React.useState([]);
+  const service = new Match();
+
+  React.useEffect(()=>{
+    const getItems = async () => {
+      let _items = await service.getMatchUser();
+      setItems(_items.response.data.matches)
+      console.log("_items",_items);
+      console.log("items",items);
+    }
+    getItems();
+  },[])
   return (
     <List
       sx={{
@@ -33,71 +47,33 @@ export default function AlignItemsList({ title }) {
       >
         {title}
       </Typography>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Sandra Adams
-              </Typography>
-              {" — Do you have Paris recommendations? Have you ever…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
+      {items.length > 0 && items.map((item)=>(
+        <>
+              <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+              </ListItemAvatar>
+              <ListItemText
+                primary={item.firstName + " " + item.lastName}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      sx={{ display: "inline" }}
+                      component="span"
+                      variant="body2"
+                      color="text.primary"
+                    >
+                      {item.email}
+                    </Typography>
+                    {" "}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            </>
+      ))}
+      
     </List>
   );
 }
---
+++ src/services/MatchService.js
@@ -0,0 +1,23 @@
+
+class Match{
+    getMatchUser = async () =>{
+        let status = 0
+        let res;
+
+        await fetch("https://api.roomie.helloworldeducation.com/api/match", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": 'Bearer '  + JSON.parse(localStorage.getItem('data')).data.accessToken
+            },
+          })
+          .then((response) => {status = response.status; return response.json()})
+          .then((result) => {
+            res = result;
+          })
+          .catch((error) => {console.log("error", error)});
+
+          return {status:status,response:res};
+      };
+}
+export default Match;
